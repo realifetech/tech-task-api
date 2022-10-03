beefc92cbdebaa4d46512ee4d4a52db7045636c0
Ross Patman <ross.patman@livestyled.com>
2022-10-03 14:12:06 +0100

    Add price to offers

--- functions/index.js
+++ functions/index.js
@@ -208,146 +208,175 @@ const DBOffers = [
     {
         "id": "5d4aab8ba707c58f2522ddd7",
         "title": "Product 1",
-        "image": "https://s3-eu-west-1.amazonaws.com/bstage/offers/PQN94H6LSZ8D0YOKCMV5.png"
+        "image": "https://s3-eu-west-1.amazonaws.com/bstage/offers/PQN94H6LSZ8D0YOKCMV5.png",
+        "price": 100
     },
     {
         "id": "5d4aab8b2ce888bcf522979d",
         "title": "Product 2",
-        "image": "https://s3-eu-west-1.amazonaws.com/bstage/offers/PQN94H6LSZ8D0YOKCMV5.png"
+        "image": "https://s3-eu-west-1.amazonaws.com/bstage/offers/PQN94H6LSZ8D0YOKCMV5.png",
+        "price": 100
     },
     {
         "id": "5d4aab8b14d671a3aa0885a4",
         "title": "Product 3",
-        "image": "https://s3-eu-west-1.amazonaws.com/bstage/offers/PQN94H6LSZ8D0YOKCMV5.png"
+        "image": "https://s3-eu-west-1.amazonaws.com/bstage/offers/PQN94H6LSZ8D0YOKCMV5.png",
+        "price": 100
     },
     {
         "id": "5d4aab8bddab2c96d47493a1",
         "title": "Product 4",
-        "image": "https://s3-eu-west-1.amazonaws.com/bstage/offers/PQN94H6LSZ8D0YOKCMV5.png"
+        "image": "https://s3-eu-west-1.amazonaws.com/bstage/offers/PQN94H6LSZ8D0YOKCMV5.png",
+        "price": 100
     },
     {
         "id": "5d4aab8b15341b636b9d41ff",
         "title": "Product 5",
-        "image": "https://s3-eu-west-1.amazonaws.com/bstage/offers/PQN94H6LSZ8D0YOKCMV5.png"
+        "image": "https://s3-eu-west-1.amazonaws.com/bstage/offers/PQN94H6LSZ8D0YOKCMV5.png",
+        "price": 100
     },
     {
         "id": "5d4aab8ba99d34b3053d1bde",
         "title": "Product 6",
-        "image": "https://s3-eu-west-1.amazonaws.com/bstage/offers/PQN94H6LSZ8D0YOKCMV5.png"
+        "image": "https://s3-eu-west-1.amazonaws.com/bstage/offers/PQN94H6LSZ8D0YOKCMV5.png",
+        "price": 100
     },
     {
         "id": "5d4aab8bf3bf9ba25aab2615",
         "title": "Product 7",
-        "image": "https://s3-eu-west-1.amazonaws.com/bstage/offers/PQN94H6LSZ8D0YOKCMV5.png"
+        "image": "https://s3-eu-west-1.amazonaws.com/bstage/offers/PQN94H6LSZ8D0YOKCMV5.png",
+        "price": 100
     },
     {
         "id": "5d4aab8b32159f510266e63c",
         "title": "Product 8",
-        "image": "https://s3-eu-west-1.amazonaws.com/bstage/offers/PQN94H6LSZ8D0YOKCMV5.png"
+        "image": "https://s3-eu-west-1.amazonaws.com/bstage/offers/PQN94H6LSZ8D0YOKCMV5.png",
+        "price": 100
     },
     {
         "id": "5d4aab8bf6700ceb24507f5a",
         "title": "Product 9",
-        "image": "https://s3-eu-west-1.amazonaws.com/bstage/offers/PQN94H6LSZ8D0YOKCMV5.png"
+        "image": "https://s3-eu-west-1.amazonaws.com/bstage/offers/PQN94H6LSZ8D0YOKCMV5.png",
+        "price": 100
     },
     {
         "id": "5d4aab8b18c4015237387361",
         "title": "Product 10",
-        "image": "https://s3-eu-west-1.amazonaws.com/bstage/offers/PQN94H6LSZ8D0YOKCMV5.png"
+        "image": "https://s3-eu-west-1.amazonaws.com/bstage/offers/PQN94H6LSZ8D0YOKCMV5.png",
+        "price": 100
     },
     {
         "id": "5d4aab8b35752bd3d7fe64c5",
         "title": "Product 11",
-        "image": "https://s3-eu-west-1.amazonaws.com/bstage/offers/PQN94H6LSZ8D0YOKCMV5.png"
+        "image": "https://s3-eu-west-1.amazonaws.com/bstage/offers/PQN94H6LSZ8D0YOKCMV5.png",
+        "price": 100
     },
     {
         "id": "5d4aab8bd67805762783fb5a",
         "title": "Product 12",
-        "image": "https://s3-eu-west-1.amazonaws.com/bstage/offers/PQN94H6LSZ8D0YOKCMV5.png"
+        "image": "https://s3-eu-west-1.amazonaws.com/bstage/offers/PQN94H6LSZ8D0YOKCMV5.png",
+        "price": 100
     },
     {
         "id": "5d4aab8b747b4d0621d78000",
         "title": "Product 13",
-        "image": "https://s3-eu-west-1.amazonaws.com/bstage/offers/PQN94H6LSZ8D0YOKCMV5.png"
+        "image": "https://s3-eu-west-1.amazonaws.com/bstage/offers/PQN94H6LSZ8D0YOKCMV5.png",
+        "price": 100
     },
     {
         "id": "5d4aab8be2568c8ac57ca8b2",
         "title": "Product 14",
-        "image": "https://s3-eu-west-1.amazonaws.com/bstage/offers/PQN94H6LSZ8D0YOKCMV5.png"
+        "image": "https://s3-eu-west-1.amazonaws.com/bstage/offers/PQN94H6LSZ8D0YOKCMV5.png",
+        "price": 100
     },
     {
         "id": "5d4aab8b033084f21bddccff",
         "title": "Product 15",
-        "image": "https://s3-eu-west-1.amazonaws.com/bstage/offers/PQN94H6LSZ8D0YOKCMV5.png"
+        "image": "https://s3-eu-west-1.amazonaws.com/bstage/offers/PQN94H6LSZ8D0YOKCMV5.png",
+        "price": 100
     },
     {
         "id": "5d4aab8bbf3bf100de94fbf0",
         "title": "Product 16",
-        "image": "https://s3-eu-west-1.amazonaws.com/bstage/offers/PQN94H6LSZ8D0YOKCMV5.png"
+        "image": "https://s3-eu-west-1.amazonaws.com/bstage/offers/PQN94H6LSZ8D0YOKCMV5.png",
+        "price": 100
     },
     {
         "id": "5d4aab8b4ada8fca84541eee",
         "title": "Product 17",
-        "image": "https://s3-eu-west-1.amazonaws.com/bstage/offers/PQN94H6LSZ8D0YOKCMV5.png"
+        "image": "https://s3-eu-west-1.amazonaws.com/bstage/offers/PQN94H6LSZ8D0YOKCMV5.png",
+        "price": 100
     },
     {
         "id": "5d4aab8bb99f6158080626a4",
         "title": "Product 18",
-        "image": "https://s3-eu-west-1.amazonaws.com/bstage/offers/PQN94H6LSZ8D0YOKCMV5.png"
+        "image": "https://s3-eu-west-1.amazonaws.com/bstage/offers/PQN94H6LSZ8D0YOKCMV5.png",
+        "price": 100
     },
     {
         "id": "5d4aab8b5a1fec498eb40bb7",
         "title": "Product 19",
-        "image": "https://s3-eu-west-1.amazonaws.com/bstage/offers/PQN94H6LSZ8D0YOKCMV5.png"
+        "image": "https://s3-eu-west-1.amazonaws.com/bstage/offers/PQN94H6LSZ8D0YOKCMV5.png",
+        "price": 100
     },
     {
         "id": "5d4aab8bf54bf176dfdbbdb1",
         "title": "Product 20",
-        "image": "https://s3-eu-west-1.amazonaws.com/bstage/offers/PQN94H6LSZ8D0YOKCMV5.png"
+        "image": "https://s3-eu-west-1.amazonaws.com/bstage/offers/PQN94H6LSZ8D0YOKCMV5.png",
+        "price": 100
     },
     {
         "id": "5d4aab8bf28ea806fc721e1b",
         "title": "Product 21",
-        "image": "https://s3-eu-west-1.amazonaws.com/bstage/offers/PQN94H6LSZ8D0YOKCMV5.png"
+        "image": "https://s3-eu-west-1.amazonaws.com/bstage/offers/PQN94H6LSZ8D0YOKCMV5.png",
+        "price": 100
     },
     {
         "id": "5d4aab8bd938be0688c0896a",
         "title": "Product 22",
-        "image": "https://s3-eu-west-1.amazonaws.com/bstage/offers/PQN94H6LSZ8D0YOKCMV5.png"
+        "image": "https://s3-eu-west-1.amazonaws.com/bstage/offers/PQN94H6LSZ8D0YOKCMV5.png",
+        "price": 100
     },
     {
         "id": "5d4aab8beb3ff19dfc947a06",
         "title": "Product 23",
-        "image": "https://s3-eu-west-1.amazonaws.com/bstage/offers/PQN94H6LSZ8D0YOKCMV5.png"
+        "image": "https://s3-eu-west-1.amazonaws.com/bstage/offers/PQN94H6LSZ8D0YOKCMV5.png",
+        "price": 100
     },
     {
         "id": "5d4aab8bb00b2a0cc510b7c1",
         "title": "Product 24",
-        "image": "https://s3-eu-west-1.amazonaws.com/bstage/offers/PQN94H6LSZ8D0YOKCMV5.png"
+        "image": "https://s3-eu-west-1.amazonaws.com/bstage/offers/PQN94H6LSZ8D0YOKCMV5.png",
+        "price": 100
     },
     {
         "id": "5d4aab8b97b7879e3a391c51",
         "title": "Product 25",
-        "image": "https://s3-eu-west-1.amazonaws.com/bstage/offers/PQN94H6LSZ8D0YOKCMV5.png"
+        "image": "https://s3-eu-west-1.amazonaws.com/bstage/offers/PQN94H6LSZ8D0YOKCMV5.png",
+        "price": 100
     },
     {
         "id": "5d4aab8b7d503641f0b00b18",
         "title": "Product 26",
-        "image": "https://s3-eu-west-1.amazonaws.com/bstage/offers/PQN94H6LSZ8D0YOKCMV5.png"
+        "image": "https://s3-eu-west-1.amazonaws.com/bstage/offers/PQN94H6LSZ8D0YOKCMV5.png",
+        "price": 100
     },
     {
         "id": "5d4aab8bc37fbb8c01c12948",
         "title": "Product 27",
-        "image": "https://s3-eu-west-1.amazonaws.com/bstage/offers/PQN94H6LSZ8D0YOKCMV5.png"
+        "image": "https://s3-eu-west-1.amazonaws.com/bstage/offers/PQN94H6LSZ8D0YOKCMV5.png",
+        "price": 100
     },
     {
         "id": "5d4aab8bcd23dee1a42c3efc",
         "title": "Product 28",
-        "image": "https://s3-eu-west-1.amazonaws.com/bstage/offers/PQN94H6LSZ8D0YOKCMV5.png"
+        "image": "https://s3-eu-west-1.amazonaws.com/bstage/offers/PQN94H6LSZ8D0YOKCMV5.png",
+        "price": 100
     },
     {
         "id": "5d4aab8bea15d40a9eb5e3aa",
         "title": "Product 29",
-        "image": "https://s3-eu-west-1.amazonaws.com/bstage/offers/PQN94H6LSZ8D0YOKCMV5.png"
+        "image": "https://s3-eu-west-1.amazonaws.com/bstage/offers/PQN94H6LSZ8D0YOKCMV5.png",
+        "price": 100
     }
 ]
